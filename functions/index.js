b15c5baca6fb1bf746cd32b8920e0628a393f870
Taeho Lee <31860067+wheels63@users.noreply.github.com>
2024-08-03 19:15:10 -0400

    firebase function deployed

--- functions/index.js
+++ functions/index.js
@@ -1,3 +1,5 @@
+const SibApiV3Sdk = require('sib-api-v3-sdk');
+
 /**
  * Import function triggers from their respective submodules:
  *
@@ -7,8 +9,8 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-const {onRequest} = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
+// const { onRequest } = require('firebase-functions/v2/https');
+// const logger = require('firebase-functions/logger');
 
 // Create and deploy your first functions
 // https://firebase.google.com/docs/functions/get-started
@@ -17,3 +19,54 @@ const logger = require("firebase-functions/logger");
 //   logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+const functions = require('firebase-functions');
+const BREVO_API_KEY = functions.config().brevo.api_key;
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+exports.sendReminderEmail = functions.pubsub
+    .schedule('every 24 hours')
+    .onRun(async (context) => {
+        const now = admin.firestore.Timestamp.now();
+        const remindersSnapshot = await admin
+            .firestore()
+            .collection('reminders')
+            .where('replacementDate', '<=', now)
+            .get();
+
+        const sendEmailPromises = remindersSnapshot.docs.map(async (doc) => {
+            const reminderData = doc.data();
+            const defaultClient = SibApiV3Sdk.ApiClient.instance;
+            const apiKey = defaultClient.authentications['api-key'];
+            apiKey.apiKey = BREVO_API_KEY;
+
+            const apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
+            const sendSmtpEmail = new SibApiV3Sdk.SendSmtpEmail();
+
+            sendSmtpEmail.subject = 'Your Water Filter Needs Replacing';
+            sendSmtpEmail.htmlContent = `<html>
+        <body><h1>Time to Replace Your Water Filter</h1>
+        <p>Your water filter is due for a replacement. 
+        Don't forget to change it to ensure clean, fresh water!</p>
+        </body></html>`;
+            sendSmtpEmail.sender = {
+                name: 'HydroFilter Reminder',
+                email: 'noreply@yourapp.com',
+            };
+            sendSmtpEmail.to = [{ email: reminderData.email }];
+
+            try {
+                const data = await apiInstance.sendTransacEmail(sendSmtpEmail);
+                console.log(
+                    'Email sent successfully. Returned data: ' +
+                        JSON.stringify(data)
+                );
+            } catch (error) {
+                console.error('Error sending email:', error);
+            }
+            console.log(`Sending reminder email to ${reminderData.email}`);
+        });
+
+        await Promise.all(sendEmailPromises);
+    });
